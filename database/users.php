0eeeea616067aa96889ff3607bffca4b462e2e0d
Jose Miguel Melo <melo199435@gmail.com>
2015-05-27 12:48:54 +0100

    Edit profile implemented

--- database/users.php
+++ database/users.php
@@ -1,27 +1,27 @@
 <?php
   
-  function createUser($username, $email, $password) {
-    global $conn;
+    function createUser($username, $email, $password) {
+        global $conn;
 
-      $role=1;
-      $referal ="";
-      $cash = 0;
-      $last_access = date("Y-m-d H:i:s");
-      $created_when = date("Y-m-d H:i:s");
+        $role=1;
+        $referal ="";
+        $cash = 0;
+        $last_access = date("Y-m-d H:i:s");
+        $created_when = date("Y-m-d H:i:s");
         $avatar = "http://heintendsvictory.org/wp-content/uploads/default-avatar.png";
 
-      $stmt = $conn->prepare("INSERT INTO \"User\" (name,email,password,ROLE,referral,cash,last_acess,avatar,created_when) VALUES (?, ?, ?, ?,?,?,?,?,?)");
-    $stmt->execute(array($username, $email, sha1($password), $role, $referal, $cash, $last_access, $avatar, $created_when));
-  }
+        $stmt = $conn->prepare("INSERT INTO \"User\" (name,email,password,ROLE,referral,cash,last_acess,avatar,created_when) VALUES (?, ?, ?, ?,?,?,?,?,?)");
+        $stmt->execute(array($username, $email, sha1($password), $role, $referal, $cash, $last_access, $avatar, $created_when));
+    }
 
-  function isLoginCorrect($email, $password) {
-    global $conn;
-    $stmt = $conn->prepare("SELECT * 
-                            FROM \"User\"
-                            WHERE email = ? AND password = ?");
-    $stmt->execute(array($email, sha1($password)));
-    return $stmt->fetch();
-  }
+    function isLoginCorrect($email, $password) {
+        global $conn;
+        $stmt = $conn->prepare("SELECT *
+                                FROM \"User\"
+                                WHERE email = ? AND password = ?");
+        $stmt->execute(array($email, sha1($password)));
+        return $stmt->fetch();
+    }
 
     function getUserById($id){
         global $conn;
@@ -31,4 +31,39 @@
         $stmt->execute();
         return $stmt->fetch();
     }
+
+    function updateUser($userId, $name, $email, $password,$avatar)
+    {
+
+        $inputArray = array();
+
+        $query = "UPDATE \"User\"
+                                SET name = ?";
+
+        $inputArray[] = $name;
+
+        if($_SESSION['user']['email'] != $email) {
+            $query = $query . ", email = ?";
+            $inputArray[] = $email;
+        }
+
+        if($password != ""){
+            $query = $query . ", password = ?";
+            $inputArray[] = sha1($password);
+        }
+        if($avatar != ""){
+            $query = $query .", avatar = ?";
+            $inputArray[] = $avatar;
+        }
+
+        $inputArray[] = $userId;
+
+        $query = $query . "WHERE id = ?";
+
+        global $conn;
+        $stmt = $conn->prepare($query);
+        $stmt->execute($inputArray);
+    }
+
+
 ?>
--- templates_c/7a1ff753288d1cc19d37ca4393ea28d3c7e65597.file.view_question.tpl.php
+++ templates_c/7a1ff753288d1cc19d37ca4393ea28d3c7e65597.file.view_question.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2015-05-20 12:17:59
+<?php /* Smarty version Smarty-3.1.15, created on 2015-05-27 13:11:36
          compiled from "/opt/lbaw/lbaw1461/public_html/melo/templates/questions/view_question.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:2121730933555c5f571fdbb5-33161168%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '7a1ff753288d1cc19d37ca4393ea28d3c7e65597' => 
     array (
       0 => '/opt/lbaw/lbaw1461/public_html/melo/templates/questions/view_question.tpl',
-      1 => 1431517520,
+      1 => 1432722665,
       2 => 'file',
     ),
   ),
@@ -15,15 +15,16 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.15',
+  'unifunc' => 'content_555c5f57323588_56351174',
   'variables' => 
   array (
     'question' => 0,
-    'answer' => 0,
     'BASE_URL' => 0,
+    'comment' => 0,
+    'answer' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.15',
-  'unifunc' => 'content_555c5f57323588_56351174',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_555c5f57323588_56351174')) {function content_555c5f57323588_56351174($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('common/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
 
@@ -47,13 +48,16 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 
     <div class="content animated zoomIn">
         <div class="row social-board">
-            <div class="col-lg-1 vote-thumbs">
-                <span class="fa fa-thumbs-o-up"></span>
+            <div class="col-lg-1 col-xs-2 vote-thumbs">
+                <span class="fa fa-thumbs-o-up" data-id="<?php echo $_smarty_tpl->tpl_vars['question']->value['question']['contentid'];?>
+" data-value="1"></span>
                 <p><?php echo $_smarty_tpl->tpl_vars['question']->value['questionVotes'];?>
 </p>
-                <span class="fa fa-thumbs-o-down"></span>
+                <span class="fa fa-thumbs-o-down" data-id="<?php echo $_smarty_tpl->tpl_vars['question']->value['question']['contentid'];?>
+" data-value="-1"></span>
+
             </div>
-            <div class="col-lg-11">
+            <div class="col-lg-11 col-xs-10">
                 <div class="hpanel hblue">
                     <div class="panel-body">
                         <div class="media social-profile clearfix">
@@ -77,7 +81,43 @@ $_valid = $_smarty_tpl->decodeProperties(array (
                     </div>
                     <div class="panel-footer">
                         <div class="social-form">
-                            <input class="form-control" placeholder="Your comment">
+                            <form action="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+actions/comments/add_comment.php" id="addCommentForm" method="post">
+                               
+                                <?php  $_smarty_tpl->tpl_vars['comment'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['comment']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['question']->value['question']['comments']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['comment']->key => $_smarty_tpl->tpl_vars['comment']->value) {
+$_smarty_tpl->tpl_vars['comment']->_loop = true;
+?>
+                                
+                                <div class="social-talk">
+                                    <div class="media social-profile clearfix">
+                                        <a class="pull-left">
+                                            <img src="<?php echo $_smarty_tpl->tpl_vars['comment']->value['avatar'];?>
+" alt="profile-picture">
+                                        </a>
+
+                                        <div class="media-body">
+                                            <span class="font-bold"><?php echo $_smarty_tpl->tpl_vars['comment']->value['name'];?>
+</span>
+                                            <small class="text-muted"><?php echo $_smarty_tpl->tpl_vars['comment']->value['created_when'];?>
+</small>
+
+                                            <div class="social-content">
+                                                <?php echo $_smarty_tpl->tpl_vars['comment']->value['content'];?>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <?php } ?>
+                                
+                                
+                                <input hidden="true" name="questionId" value="<?php echo $_smarty_tpl->tpl_vars['question']->value['question']['questionid'];?>
+"/>
+                                <input name="commentContent" class="form-control comment" placeholder="Your comment">
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -94,14 +134,15 @@ foreach ($_from as $_smarty_tpl->tpl_vars['answer']->key => $_smarty_tpl->tpl_va
 $_smarty_tpl->tpl_vars['answer']->_loop = true;
 ?>
             <div class="row social-board">
-                <div class="col-lg-1 vote-thumbs">
-                    <span class="fa fa-thumbs-o-up"></span>
-
+                <div class="col-lg-1 col-xs-2 vote-thumbs">
+                    <span class="fa fa-thumbs-o-up" data-id="<?php echo $_smarty_tpl->tpl_vars['answer']->value['contentid'];?>
+" data-value="1"></span>
                     <p><?php echo $_smarty_tpl->tpl_vars['answer']->value['classification'];?>
 </p>
-                    <span class="fa fa-thumbs-o-down"></span>
+                    <span class="fa fa-thumbs-o-down" data-id="<?php echo $_smarty_tpl->tpl_vars['answer']->value['contentid'];?>
+" data-value="-1"></span>
                 </div>
-                <div class="col-lg-11">
+                <div class="col-lg-11 col-xs-10">
                     <div class="hpanel hblue">
                         <div class="panel-body">
                             <div class="media social-profile clearfix">
@@ -122,10 +163,46 @@ $_smarty_tpl->tpl_vars['answer']->_loop = true;
                                 <?php echo $_smarty_tpl->tpl_vars['answer']->value['content'];?>
 
                             </div>
+
                         </div>
                         <div class="panel-footer">
                             <div class="social-form">
-                                <input class="form-control" placeholder="Your comment">
+                                <form action="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+actions/comments/add_comment.php" id="addCommentForm" method="post">
+                                    <?php  $_smarty_tpl->tpl_vars['comment'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['comment']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['answer']->value['comments']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['comment']->key => $_smarty_tpl->tpl_vars['comment']->value) {
+$_smarty_tpl->tpl_vars['comment']->_loop = true;
+?>
+                                
+                                        <div class="social-talk">
+                                            <div class="media social-profile clearfix">
+                                                <a class="pull-left">
+                                                    <img src="<?php echo $_smarty_tpl->tpl_vars['comment']->value['avatar'];?>
+" alt="profile-picture">
+                                                </a>
+        
+                                                <div class="media-body">
+                                                    <span class="font-bold"><?php echo $_smarty_tpl->tpl_vars['comment']->value['name'];?>
+</span>
+                                                    <small class="text-muted"><?php echo $_smarty_tpl->tpl_vars['comment']->value['created_when'];?>
+</small>
+        
+                                                    <div class="social-content">
+                                                        <?php echo $_smarty_tpl->tpl_vars['comment']->value['content'];?>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    
+                                    <?php } ?>
+                                    
+                                    
+                                    <input hidden="true" name="answerId" value="<?php echo $_smarty_tpl->tpl_vars['answer']->value['contentid'];?>
+"/>
+                                    <input name="commentContent" class="form-control comment" placeholder="Your comment">
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -133,30 +210,33 @@ $_smarty_tpl->tpl_vars['answer']->_loop = true;
             </div>
         <?php } ?>
 
-
-
-        <div class="row">
-            <div class="col-lg-12 animated-panel zoomIn" style="-webkit-animation-delay: 0.2s;">
-                <div class="hpanel">
-                    <div class="panel-heading">
-                        Your Answer
-                    </div>
-                    <div class="panel-body">
-                        <form action="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+        <?php if ($_smarty_tpl->tpl_vars['question']->value['question']['closed']===false) {?>
+            <div class="row">
+                <div class="col-lg-12 animated-panel zoomIn" style="-webkit-animation-delay: 0.2s;">
+                    <div class="hpanel">
+                        <div class="panel-heading">
+                            Your Answer
+                        </div>
+                        <div class="panel-body">
+                            <form action="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 actions/questions/add_answer.php" id="addAnswerForm" method="post">
-                            <input hidden="true" name="questionId" value="<?php echo $_smarty_tpl->tpl_vars['question']->value['question']['questionid'];?>
+                                <input hidden="true" name="questionId" value="<?php echo $_smarty_tpl->tpl_vars['question']->value['question']['questionid'];?>
 "/>
-                            <textarea name="answerContent" class="form-control" rows="3"></textarea>
-                            <br/>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                        </form>
+                                <textarea name="answerContent" class="form-control" rows="3"></textarea>
+                                <br/>
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </form>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        <?php }?>
+
 
     </div>
 
 <?php echo $_smarty_tpl->getSubTemplate ('common/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+
 <?php }} ?>
