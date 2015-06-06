a3d9d1149b8195a6fc77d04c25a05af18404db1e
ricardojrloureiro <ricardojrloureiro@gmail.com>
2015-06-06 01:52:43 +0100

    js update

--- javascript/messages.js
+++ javascript/messages.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
-    var errors=0;
+$(document).ready(function () {
+    var errors = 0;
+    var messagesDeleted = 0;
 
     $(".fa-trash").click(function () {
         var contentid = $(this).data("id");
-        var tableRow = $("#"+contentid);
-        console.log("Value",contentid);
+        var tableRow = $("#" + contentid);
+        console.log("Value", contentid);
 
         $.ajax({
             url: BASE_URL + "actions/messages/delete.php",
@@ -13,6 +14,18 @@ $(document).ready(function() {
             success: function (data, textStatus, jqXHR) {
                 if (data.error == false) {
                     tableRow.remove();
+                    if (messagesDeleted == 0) {
+                        var succDiv = $("#success_messages");
+                        console.log(succDiv);
+                        if (succDiv.length) {
+                            succDiv.after("<div class='alert alert-success'>Message deleted</div>");
+                            messagesDeleted++;
+                        } else {
+                            var errorDiv = $(".normalheader");
+                            errorDiv.before("<div class='normalheader'> <div id='success_messages'><div class='alert alert-success'>Message deleted</div></div></div>");
+                            messagesDeleted++;
+                        }
+                    }
                 }
             },
             error: function (data, textStatus, errorThrown) {
@@ -26,9 +39,6 @@ $(document).ready(function() {
         var to_id = $("#to_email").val();
         var subject = $("#subject").val();
         var message = $("#message").val();
-        console.log(to_id);
-        console.log(subject);
-        console.log(message);
 
         $.ajax({
             url: BASE_URL + "actions/messages/create.php",
@@ -40,11 +50,11 @@ $(document).ready(function() {
             },
             success: function (data, textStatus, jqXHR) {
                 console.log("Success");
-                window.location.replace(BASE_URL+"/pages/users/messages.php");
+                window.location.replace(BASE_URL + "pages/users/messages.php");
             },
             error: function (data, textStatus, jqXHR) {
                 console.log(data);
-                if(errors==0) {
+                if (errors == 0) {
                     var errorDiv = $(".normalheader");
                     errorDiv.after("<div class='normalheader'> <div id='error_messages'><div class='alert alert-danger'>The destination user does not exists</div></div></div>");
                     errors++;
