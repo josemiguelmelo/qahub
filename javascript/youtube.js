47ec5996bde07293c3bcbfb0f903aa3679afa0c1
ricardojrloureiro <ricardojrloureiro@gmail.com>
2015-06-08 14:30:33 +0100

    Images javascript in questions

--- javascript/youtube.js
+++ javascript/youtube.js
@@ -13,6 +13,15 @@ $(document).ready(function() {
             $(".youtube-player").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + youtubeId + '" frameborder="0" allowfullscreen></iframe>');
             $(".youtube-player").show();
         }
+
+        var imageUrl = linkifyImages(questionContent);
+        if(imageUrl != null)
+        {
+            imageUrl = imageUrl[1];
+            $(".youtube-player").append('<img class="img-responsive" src="' + imageUrl + '" alt="Image"/>');
+            $(".youtube-player").show();
+
+        }
     }
 
 
@@ -20,6 +29,12 @@ $(document).ready(function() {
 
 function linkifyYouTubeURLs(text) {
     var regex = /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig;
+    var pattern = new RegExp(regex);
+    return pattern.exec(text);
+}
+
+function linkifyImages(text) {
+    var regex = /(https?:\/\/.*\.(?:png|jpg|gif|jpeg))/ig;
     var pattern = new RegExp(regex);
     return pattern.exec(text);
 }
