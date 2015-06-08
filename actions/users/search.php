589bcbfaee162f190fec732246a9ea447c6785cf
ricardojrloureiro <ricardojrloureiro@gmail.com>
2015-06-08 14:24:24 +0100

    Search users admin

---
+++ actions/users/search.php
@@ -0,0 +1,19 @@
+<?php
+include_once('../../config/init.php');
+include_once($BASE_DIR .'database/users.php');
+include_once($BASE_DIR.'database/messages.php');
+
+checkIfLoggedIn();
+
+$userAdmin = checkAdmin($_SESSION['user']['id']);
+
+$users = getUsersByString($_POST['search']);
+
+$numberOfMessages = getUserMessages($_SESSION['user']['id']);
+
+$smarty->assign('admin',$userAdmin);
+$smarty->assign('numberOfMessages',$numberOfMessages);
+$smarty->assign('all_users',$users);
+$smarty->display('users/view_users.tpl');
+
+
